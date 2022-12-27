15c0d7ded69f89d5870bff27ab11392bc5e2fccd
esteban ortiz <ztef@hotmail.es>
2022-12-27 16:46:21 +0000

    Merge branch 'main' of https://github.com/ztef/smartpump into main

--- client/src/blocs/AppBloc.ts
+++ client/src/blocs/AppBloc.ts
@@ -65,7 +65,7 @@ class AppBloc extends Bloc<AppState> {
 
    async getCredentials(credentials: any) {
    
-        let api_url = `http://localhost:5000/api/log_user?email=${credentials.username}&password=${credentials.password}`;
+        let api_url = `http://ztef-orange-telegram-p7jjgwrq4gc9774-5000.preview.app.github.dev/api/log_user?email=${credentials.username}&password=${credentials.password}`;
         try {
             const res = await axios.get(api_url);
             var response = res;
@@ -83,7 +83,7 @@ class AppBloc extends Bloc<AppState> {
 
     async sendUpdate(user_data: any) {
    
-        let api_url = `http://localhost:5000/api/update_user`;
+        let api_url = `http://ztef-orange-telegram-p7jjgwrq4gc9774-5000.preview.app.github.dev/api/update_user`;
         try {
             const res = await axios.post(api_url,user_data);
             var response = res;
